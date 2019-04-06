29969b702661ebe025221dece0cc8db8ad5668c2
corneliu10 <corneliu.dcv@gmail.com>
2019-04-06 15:21:39 +0300

    renamed lab3 IA

--- Anul II/IA/lab3/8-puzzle/8_puzzle_lab.py
+++ Anul II/IA/lab3/8-puzzle/8_puzzle_lab.py
@@ -1,7 +1,10 @@
+import random
+import ai_8_puzzle as db
 import pygame
 
 pygame.init()
 screen_width_height = 600
+
 win = pygame.display.set_mode((screen_width_height, screen_width_height))
 pygame.display.set_caption("8-puzzle")
 
@@ -10,36 +13,45 @@ y_new = 0
 width = 200
 height = 200
 vel = 200
-pozitii = [2, 4, 3, 8, 7, 5, 1, 0, 6]
-zero = pozitii.index(0)
-#new_char = zero
-# determinati coordonatele x si y la care va fi plasata placuta goala, stiind ca zero este indexul unde se afla aceasta pozitie in "pozitii"
-# si stiind ca inaltilea si latimea unei placute este de width si height si ca dimensiunea ferestrei este screen_width_height
-if zero < 3:
-    x = zero * 200
-    y = 0
-elif 3 <= zero < 6:
-    ...
-else:
-    ...
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+red = (200, 0, 0)
+green = (0, 200, 0)
+
+bright_red = (255, 0, 0)
+bright_green = (0, 255, 0)
+
+
+def get_x_y(zero):
+    if zero < 3:
+        x = zero * 200
+        y = 0
+    elif 3 <= zero < 6:
+        x = 200 * zero - 600
+        y = 200
+    else:
+        x = 200 * zero - 1200
+        y = 400
+    return x, y
+
 
 number_list = [pygame.image.load('9.png'), pygame.image.load('1.png'), pygame.image.load('2.png'),
                pygame.image.load('3.png'), pygame.image.load('4.png'), pygame.image.load('5.png'),
                pygame.image.load('6.png'), pygame.image.load('7.png'), pygame.image.load('8.png')]
 
 
-def draw_game_window():
+def draw_game_window(pozitii):
+    print(pozitii)
     for i in range(len(pozitii)):
         if i < 3:
-			# comanda win.blit afiseaza pe ecran imaginea data ca prim argument la pozitiile date a argument secund
             win.blit(number_list[pozitii[i]], (200 * i, 0))
         elif 3 <= i < 6:
-            ...
+            win.blit(number_list[pozitii[i]], (200 * i - 600, 200))
         else:
-            ...
-
-
-draw_game_window()
+            win.blit(number_list[pozitii[i]], (200 * i - 1200, 400))
+    pygame.display.update()
 
 
 def rewrite_list(ind1, ind2, lnou):
@@ -47,54 +59,167 @@ def rewrite_list(ind1, ind2, lnou):
     return lnou
 
 
-def redraw_game_window(pozitii, x, y, zero, left, right, up, down):
+def redraw_game_window(pozitii, x, y, x_old, y_old, zero, left, right, up, down, calculator):
     i = int((y / 200) * 3 + (x / 200))
-    print("i: ", i)
+    pygame.time.delay(10)
+    # print("i: ", i)
 
-    if left:
-        #print('left x,y, zero', x, y, zero)
+    if left or right or up or down:
         win.blit(number_list[0], (x, y))
         win.blit(number_list[pozitii[i]], (x_old, y_old))
-    elif right:
-        ...
-    elif up:
-        ...
-    elif down:
-        ...
-		
-    pozitii = rewrite_list(zero, i, pozitii)
+
     pygame.display.update()
 
 
-run = True
+def is_equal(L1, L2):
+    for i in range(len(L1)):
+        if L1[i] != L2[i]:
+            return False
+    return True
 
-while run:
-    pygame.time.delay(100)
-    zero = pozitii.index(0)
-    x_old = x
-    y_old = y
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+
+def human_player(pozitii, scop):
+    draw_game_window(pozitii)
+    calculator = False
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        pygame.time.delay(100)
+        zero = pozitii.index(0)
+        x, y = get_x_y(zero)
+        x_old = x
+        y_old = y
+        print(x, y, y_old, x_old)
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and x >= vel:
+            x = x - vel
+            left, right, up, down = True, False, False, False
+            zero -= 1
+        elif keys[pygame.K_RIGHT] and x < screen_width_height - vel:
+            x = x + vel
+            left, right, up, down = False, True, False, False
+            zero += 1
+        elif keys[pygame.K_UP] and y >= vel:
+            y = y - vel
+            left, right, up, down = False, False, True, False
+            zero -= 3
+        elif keys[pygame.K_DOWN] and y < screen_width_height - vel:
+            y = y + vel
+            left, right, up, down = False, False, False, True
+            zero += 3
+        else:
+            left, right, up, down = False, False, False, False
+        print(x, y)
+        redraw_game_window(pozitii, x, y, x_old, y_old, zero, left, right, up, down, calculator)
+        i = int((y_old / 200) * 3 + (x_old / 200))
+        pozitii = rewrite_list(zero, i, pozitii)
+        if is_equal(pozitii, scop):
             run = False
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and x >= vel:
-        x = x - vel
-        left, right, up, down = True, False, False, False
-        zero -= 1
+    start_page(win,  pozitii, scop)
 
-    elif keys[pygame.K_RIGHT] and x < screen_width_height - vel:
-        ...
 
-    elif keys[pygame.K_UP] and y >= vel:
-        ...
+def computer_player(pozitii, scop=None):
+    calculator = True
+    draw_game_window(pozitii)
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        # determine if the AI chose left, right, up or down
+        list_of_moves = db.main(pozitii, scop)
+        first_move = list_of_moves[0].copy()
+        list_of_moves.pop(0)
+        draw_game_window(first_move)
+        pygame.time.delay(10)
+        pos_zero_ini = first_move.index(0)
+        x, y = get_x_y(pos_zero_ini)
+        for move in list_of_moves:
+            pygame.time.delay(1000)
+            x_old, y_old = x, y
+            pos_zero_new = move.index(0)
+            left, right, up, down = False, False, False, False
+            if pos_zero_ini - pos_zero_new == 1:
+                left, right, up, down = True, False, False, False
+            elif pos_zero_ini - pos_zero_new == -1:
+                left, right, up, down = False, True, False, False
+            elif pos_zero_ini - pos_zero_new == 3:
+                left, right, up, down = False, False, True, False
+            elif pos_zero_ini - pos_zero_new == -3:
+                left, right, up, down = False, False, False, True
+            x, y = get_x_y(pos_zero_new)
 
-    elif keys[pygame.K_DOWN] and y < screen_width_height - vel:
-        ...
+            redraw_game_window(first_move, x, y, x_old, y_old, pos_zero_ini, left, right, up, down, calculator)
 
+            pos_zero_ini = pos_zero_new
+            first_move = move.copy()
+        run = False
+        pygame.time.delay(1000)
+    start_page(win, pozitii, scop)
+
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+
+def button(msg, x, y, w, h, ic, ac, action=None, param1=None, param2=None, param3 = None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    if x + w > mouse[0] > x and y + h > mouse[1] > y:
+        pygame.draw.rect(win, ac, (x, y, w, h))
+        if click[0] == 1 and action != None and param1 != None and param2 != None and param3 == None:
+            action(param1, param2)
+        elif click[0] == 1 and action != None and param1 != None and param2 != None and param3 != None:
+            action(param1, param2, param3)
     else:
-        left, right, up, down = False, False, False, False
+        pygame.draw.rect(win, ic, (x, y, w, h))
+    smallText = pygame.font.SysFont("comicsansms", 20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ((x + (w / 2)), (y + (h / 2)))
+    win.blit(textSurf, textRect)
 
-    print(pozitii)
-    redraw_game_window(pozitii, x, y, zero, left, right, up, down)
 
+def start_page(win, pozitii, scop):
+    start_page_boolean = True
+    while start_page_boolean:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                start_page_boolean = False
+
+        win.fill(white)
+
+        largeText = pygame.font.SysFont("comicsansms", 30)
+        smallText = pygame.font.SysFont("comicsansms", 20)
+        TextSurf0, TextRect0 = text_objects("START: ", smallText)
+        win.blit(TextSurf0, (0, 0))
+        for i in range(3):
+            string = ""
+            for j in range(3):
+                string +=str(pozitii[i*3+j]) + "   "
+            TextSurf1, TextRect1 = text_objects(string, largeText)
+            win.blit(TextSurf1, (0, 20 + i * 40))
+
+        TextSurf0, TextRect0 = text_objects("SCOP: ", smallText)
+        win.blit(TextSurf0, (0, 210))
+        for i in range(3):
+            string = ""
+            for j in range(3):
+                string += str(scop[i * 3 + j]) + "   "
+            TextSurf1, TextRect1 = text_objects(string, largeText)
+            win.blit(TextSurf1, (0, 240 + i * 40))
+
+        button("Computer player", 250, 300, 300, 50, green, bright_green, computer_player, param1=pozitii, param2=scop)
+        button("Human player", 250, 400, 300, 50, red, bright_red, human_player, param1=pozitii, param2=scop)
+        pygame.display.update()
+
+
+pozitii = [2, 4, 3, 8, 7, 5, 1, 0, 6]
+scop = [1, 2, 3, 4, 5, 6, 7, 8, 0]
+start_page(win, pozitii, scop)
 pygame.quit()
---
+++ Anul II/IA/lab3/8-puzzle/ai_8_puzzle.py
@@ -0,0 +1,209 @@
+""" definirea problemei """
+
+class Nod:
+    NR_LINII = 3
+    NR_COLOANE = 3
+
+    def __init__(self, info, h):
+        self.info = info
+        self.h = 0
+
+    def __str__(self):
+        sir = "\n"
+        for i in range(0, self.__class__.NR_LINII):
+            for j in range(0, self.__class__.NR_COLOANE):
+                sir += str(self.info[i * self.__class__.NR_COLOANE + j]) + " "
+            sir += "\n"
+        return sir
+
+    def __repr__(self):
+        sir = "\n"
+        for i in range(0, Nod.NR_LINII):
+            for j in range(0, self.__class__.NR_COLOANE):
+                sir += str(self.info[i * self.__class__.NR_COLOANE + j]) + " "
+            sir += "\n"
+        return sir
+
+
+class Graf:
+
+    def __init__(self, pozitii, scop):
+
+        self.nod_start = Nod(list(pozitii), float('inf'))
+        self.reprez_scop = list(scop)
+
+    def scop(self, nod):
+        return nod.info == self.reprez_scop
+
+    def calculeaza_h(self, reprez_nod):
+        h = 0
+        for ind in range(len(reprez_nod)):
+            i_elem = ind / Nod.NR_COLOANE
+            j_elem = ind % Nod.NR_COLOANE
+            h += self.__class__.dist_hamilton(reprez_nod[ind], i_elem, j_elem, self.reprez_scop)
+        return h
+
+    def cauta_nod_nume(self, info):
+        for nod in self.noduri:
+            if nod.info == info:
+                return nod
+        return None
+
+    def dist_hamilton(elem, i_elem, j_elem, reprezentare):
+        nr = reprezentare.index(elem)
+        i_scop = nr / Nod.NR_COLOANE
+        j_scop = nr % Nod.NR_COLOANE
+        return abs(i_scop - i_elem) + abs(j_scop - j_elem)
+
+    def interschimba(ind1, ind2, l):
+        lnou = list(l)
+        lnou[ind1], lnou[ind2] = lnou[ind2], lnou[ind1]
+        return lnou
+
+    def calculeaza_succesori(self, nod):
+        ind_gol = nod.info.index(0)
+        linie_gol = int(ind_gol / Nod.NR_COLOANE)
+        coloana_gol = int(ind_gol % Nod.NR_COLOANE)
+        l_succesori = []
+        # TO DO infrumusetat cod
+        if linie_gol >= 1:
+            poz_mutare = (linie_gol - 1) * Nod.NR_COLOANE + coloana_gol
+            reprez_noua = self.__class__.interschimba(ind_gol, poz_mutare, nod.info)
+            h_nod = self.calculeaza_h(reprez_noua)
+            l_succesori.append((Nod(reprez_noua, h_nod), 1))
+        if linie_gol <= 1:
+            poz_mutare = (linie_gol + 1) * Nod.NR_COLOANE + coloana_gol
+            reprez_noua = self.__class__.interschimba(ind_gol, poz_mutare, nod.info)
+            h_nod = self.calculeaza_h(reprez_noua)
+            l_succesori.append((Nod(reprez_noua, h_nod), 1))
+        if coloana_gol >= 1:
+            poz_mutare = linie_gol * Nod.NR_COLOANE + coloana_gol - 1
+            reprez_noua = self.__class__.interschimba(ind_gol, poz_mutare, nod.info)
+            h_nod = self.calculeaza_h(reprez_noua)
+            l_succesori.append((Nod(reprez_noua, h_nod), 1))
+        if coloana_gol <= 1:
+            poz_mutare = linie_gol * Nod.NR_COLOANE + coloana_gol + 1
+            reprez_noua = self.__class__.interschimba(ind_gol, poz_mutare, nod.info)
+            h_nod = self.calculeaza_h(reprez_noua)
+            l_succesori.append((Nod(reprez_noua, h_nod), 1))
+        return l_succesori
+
+
+""" Sfarsit definire problema """
+
+""" Clase folosite in algoritmul A* """
+
+
+class NodCautare:
+    def __init__(self, nod_graf, succesori=[], parinte=None, g=0, f=None):
+        self.nod_graf = nod_graf
+        self.succesori = succesori
+        self.parinte = parinte
+        self.g = g
+        if f is None:
+            self.f = self.g + self.nod_graf.h
+        else:
+            self.f = f
+
+    def drum_arbore(self):
+        nod_c = self
+        drum = [nod_c]
+        while nod_c.parinte is not None:
+            drum = [nod_c.parinte] + drum
+            nod_c = nod_c.parinte
+        return drum
+
+    def contine_in_drum(self, nod):
+        nod_c = self
+        while nod_c.parinte is not None:
+            if nod.info == nod_c.nod_graf.info:
+                return True
+            nod_c = nod_c.parinte
+        return False
+
+    def __str__(self):
+        parinte = self.parinte if self.parinte is None else self.parinte.nod_graf.info
+        # return "("+str(self.nod_graf)+", parinte="+", f="+str(self.f)+", g="+str(self.g)+")";
+        return str(self.nod_graf)
+
+
+""" Algoritmul A* """
+
+
+def debug_str_l_noduri(l):
+    sir = ""
+    for x in l:
+        sir += str(x) + "\n"
+
+    return sir
+
+def get_lista_solutii(l):
+    """ added by Cristina """
+    drum = []
+    for x in l:
+         drum.append(x.nod_graf.info)
+    return drum
+
+def in_lista(l, nod):
+    """
+    if nod in l:
+        return true
+    """
+    for x in l:
+        if x.nod_graf.info == nod.info:
+            return x
+    return None
+
+
+
+def a_star(graf):
+    rad_arbore = NodCautare(nod_graf=graf.nod_start)
+    print(graf.nod_start.info)
+    print(graf.nod_start)
+
+    open = [rad_arbore]
+    closed = []
+    while len(open) > 0:
+        nod_curent = open.pop(0)
+        closed.append(nod_curent)
+        if graf.scop(nod_curent.nod_graf):
+            break
+        l_succesori = graf.calculeaza_succesori(nod_curent.nod_graf)
+        for (nod, cost) in l_succesori:
+            if (not nod_curent.contine_in_drum(nod)):
+                # verific daca se afla in open
+                x = in_lista(closed, nod)
+                g_succesor = nod_curent.g + cost
+                f = g_succesor + nod.h
+                if x is not None:
+                    if (f < nod_curent.f):
+                        nod_curent.f = f
+                        x.parinte = nod_curent
+                        x.g = g_succesor
+                        x.f = f
+                else:
+                    x = in_lista(open, nod)
+                    if x is not None:
+                        if (x.g > g_succesor):
+                            nod_curent.f = f
+                            x.parinte = nod_curent
+                            x.g = g_succesor
+                            x.f = f
+                    else:  # cand nu e nici in closed nici in open
+                        nod_cautare = NodCautare(nod_graf=nod, parinte=nod_curent,
+                                                 g=g_succesor)  # se calculeaza f automat in constructor
+                        open.append(nod_cautare)
+        open.sort(key=lambda x: (x.f, -x.g))
+
+
+    print("-----------------------------------------")
+    print("Drum de cost minim:" + debug_str_l_noduri(nod_curent.drum_arbore()))
+
+    """ added by Cristina: """
+    return get_lista_solutii(nod_curent.drum_arbore())
+
+
+
+def main(pozitii, scop):
+    problema = Graf(pozitii, scop)
+    return a_star(problema)
